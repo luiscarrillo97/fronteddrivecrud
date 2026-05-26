a6e55695447f7334a8303761bf1159c34b954179
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2026-05-25 21:04:00 -0500

    chore: cambiar mensaje de bienvenida a Hola Pe

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,10 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ fetch }) => {
 	try {
 		// Tu API de Cloud Run
-		const response = await fetch('https://drivecrud-git-269414280318.europe-west1.run.app/');
+		//const response = await fetch('https://drivecrud-git-269414280318.europe-west1.run.app/');
+		// Cambia la línea del fetch para apuntar a tu máquina:
+		//const response = await fetch('http://localhost:5272/');
+		const response = await fetch('https://drivecrud-269414280318.europe-west1.run.app/');
 		const mensaje = await response.text();
 
 		return {
