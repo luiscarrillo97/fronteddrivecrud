a2e49f63c5839302dd6b7d9f8bff283a429d6a5d
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2026-06-03 22:09:04 -0500

    Mi primer despliegue

--- src/routes/api/[id]/[id]/+server.ts
+++ src/routes/api/[id]/[id]/+server.ts
@@ -2,28 +2,14 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 const API_URL = 'https://drivecrud-269414280318.europe-west1.run.app';
 
-// Agregamos 'cookies' a los parámetros desestructurados
-export const GET: RequestHandler = async ({ params, fetch, cookies }) => {
+export const GET: RequestHandler = async ({ params, fetch }) => {
 	const id = params.id;
-
-	// 1. Leer el token de la sesión del usuario
-	const token = cookies.get('token');
 
 	if (!id) {
 		return new Response('ID requerido', { status: 400 });
 	}
 
-	// 2. Si alguien intenta ver el PDF directamente por URL sin estar logueado, lo bloqueamos
-	if (!token) {
-		return new Response('No autorizado. Debes iniciar sesión.', { status: 401 });
-	}
-
-	// 3. Hacemos la petición al backend enviando la llave (Token)
-	const response = await fetch(`${API_URL}/files/${id}/stream`, {
-		headers: {
-			Authorization: `Bearer ${token}` // ¡Aquí está la magia de seguridad!
-		}
-	});
+	const response = await fetch(`${API_URL}/files/${id}/stream`);
 
 	if (!response.ok) {
 		return new Response('No se pudo obtener el archivo', { status: response.status });
